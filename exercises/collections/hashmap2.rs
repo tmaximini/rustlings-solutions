48999cfe026e8698e7c176578792722adeae5b25
Thomas Maximini <tmaximini@gmail.com>
2021-12-21 17:43:12 +0100

    modules, collections

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -38,6 +38,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        if basket.contains_key(&fruit) {
+            println!("Already in basket")
+        } else {
+            basket.insert(fruit, 4);
+        }
     }
 }
 
--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,12 +7,11 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
-
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for i in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
+    // https://doc.rust-lang.org/stable/std/slice/struct.IterMut.html
+    for element in v.iter_mut() {
+        // use pointer ref (?) here
+        *element *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
