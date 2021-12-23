f2b24f8d3a7a707076010c2150a7e8c2b0828859
Thomas Maximini <tmaximini@gmail.com>
2021-12-23 10:33:35 +0100

    option and ref

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -10,8 +10,6 @@
 
 // Execute 'rustlings hint generics3' for hints!
 
-// I AM NOT DONE
-
 pub struct ReportCard<E> {
     pub grade: E,
     pub student_name: String,
--- exercises/option/option3.rs
+++ exercises/option/option3.rs
@@ -1,8 +1,6 @@
 // option3.rs
 // Make me compile! Execute `rustlings hint option3` for hints
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -11,8 +9,9 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
+    // https://doc.rust-lang.org/std/keyword.ref.html#-vs-ref
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
     y; // Fix without deleting this line.
