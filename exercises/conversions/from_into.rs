5abbbcd22a16ef910ab7228c496fe1e3d37acaac
Thomas Maximini <tmaximini@gmail.com>
2021-12-29 17:30:26 +0100

    from into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,33 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 || !s.contains(",") {
+            return Person::default();
+        }
+        let mut split = s.split(",");
+
+        let name = split.next();
+
+        if name.is_none() || name.unwrap().len() == 0 {
+            return Person::default();
+        }
+
+        let age = split.next();
+        let age_parsed = match age.unwrap().parse::<usize>() {
+            Ok(number) => number,
+            Err(e) => return Person::default(),
+        };
+
+        if age.is_none() || split.next().is_some() {
+            return Person::default();
+        }
+
+        Person {
+            name: name.unwrap().to_string(),
+            age: age_parsed,
+        }
     }
 }
 
